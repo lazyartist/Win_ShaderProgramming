d2e4855a4a2157239784a3930ca6cd0baf1c7282
y <a@a.com>
2022-07-19 08:09:40 +0900

    # 02 ColorShader - RenderMonkey

---
+++ 02_ColorShader/PS.hlsl
@@ -0,0 +1,4 @@
+float4 ps_main() : COLOR // COLOR(이 함수의 반환값을 백버퍼의 색상 값으로 처리할 것)
+{
+   return float4(1.0f, 0.0f, 0.0f, 0.0f);
+}
---
+++ 02_ColorShader/VS.hlsl
@@ -0,0 +1,23 @@
+float4x4 gWorldMatrix;
+float4x4 gViewMatrix;
+float4x4 gProjectionMatrix;
+
+struct VS_INPUT
+{
+   float4 mPosition : POSITION;
+}; // 세미콜론 없으면 "syntax error: unexpected token 'struct'" 에러 난다.
+
+struct VS_OUTPUT
+{
+   float4 mPosition : POSITION;
+};
+
+VS_OUTPUT vs_main( VS_INPUT Input )
+{
+   VS_OUTPUT Output;
+   Output.mPosition = mul( Input.mPosition, gWorldMatrix );
+   Output.mPosition = mul( Output.mPosition, gViewMatrix );
+   Output.mPosition = mul( Output.mPosition, gProjectionMatrix );
+
+   return Output;
+}
